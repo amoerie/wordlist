1e027fd30a8b088ea712f27dfda0c9b4cebb36d1
Alexander Moerman <alexander.moerman@gmail.com>
2023-05-26 10:10:28 +0200

    Move to .NET 7

---
+++ Wordlist/WordGraph.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace Wordlist;
+
+public class WordGraph
+{
+    public char Char { get; }
+    public int Length { get; set; }
+    public IDictionary<char, WordGraph> Nodes;
+
+    public WordGraph(int length, char character)
+    {
+        Length = length;
+        Char = character;
+        Nodes = new Dictionary<char, WordGraph>();
+    }
+
+    public WordGraph GetOrCreateNode(char character)
+    {
+        if (!Nodes.TryGetValue(character, out WordGraph wordGraph))
+        {
+            Nodes[character] = wordGraph = new WordGraph(Length - 1, character);
+        }
+
+        return wordGraph;
+    }
+
+    public override string ToString() => Char.ToString();
+}
---
+++ Wordlist/WordlistReader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace Wordlist;
+
+public static class WordlistReader
+{
+    public static IEnumerable<string> ReadWords()
+    {
+        var assembly = Assembly.GetExecutingAssembly();
+        var resourceName = "Wordlist.wordlist.txt";
+
+        using var stream = assembly.GetManifestResourceStream(resourceName);
+        if (stream == null)
+        {
+            throw new InvalidOperationException("Wordlist not properly embedded");
+        }
+        
+        using var reader = new StreamReader(stream, Encoding.UTF8);
+        while (reader.ReadLine() is { } line)
+        {
+            yield return line;
+        }
+    }
+
+}
